ad03391fb21ea3d5d88ca9853c6096b400a0b24f
Arthur Duarte <arthurddduarte86@gmail.com>
2022-12-09 23:11:51 -0300

    new

--- Extra/if-if-else-nested-if-if-else-if.go
+++ Extra/if-if-else-nested-if-if-else-if.go
@@ -1,13 +1,15 @@
 package main
 
-// func main() {
+import "fmt"
 
-// 	// local variable
-// 	var v int = 700
+func main() {
 
-// 	if v < 1000 {
-// 		fmt.Printf("V is less than 1000\n")
-// 	}
-// 	fmt.Printf("Value of v is : %d\n", v)
+	// local variable
+	var v int = 700
 
-// }
+	if v < 1000 {
+		fmt.Printf("V is less than 1000\n")
+	}
+	fmt.Printf("Value of v is : %d\n", v)
+
+}
